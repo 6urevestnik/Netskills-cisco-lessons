fcbf2b7900ed728fb38214eac8b945a9d023b901
6urevestnik <dangrace6q@gmail.com>
2025-07-02 02:01:33 +0700

    урок 13 готов на половину, добавил .pkt, log.txt, скрины

--- new_lesson.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-LESSON_NAME="$1"
-mkdir -p "$LESSON_NAME"/{docs,files,screenshots}
-touch "$LESSON_NAME"/{README.md,report.md}
-echo "# Урок $LESSON_NAME" > "$LESSON_NAME/README.md" 
-echo "# Отчёт по уроку $LESSON_NAME" > "$LESSON_NAME/report.md"
-echo "Урок 'LESSON_NAME' создан!"
